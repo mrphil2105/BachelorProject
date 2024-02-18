711b2dc437a5a33b13e33573a5e9166639b98e84
Philip Mørch <mrphil2105@gmail.com>
2024-02-18 12:55:28 +0100

    feat: add guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Unable to read input.");
+        let guess: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Your guess is too small."),
+            Ordering::Greater => println!("Your guess is too big."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
