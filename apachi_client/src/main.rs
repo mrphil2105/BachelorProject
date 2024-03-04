b56b01adf839819ef94f8a7abf9fb7819486da85
Philip Mørch <mrphil2105@gmail.com>
2024-03-04 12:01:40 +0100

    this does not work

--- apachi_client/src/main.rs
+++ apachi_client/src/main.rs
@@ -1,3 +1,10 @@
+mod proto {
+    pub mod submission;
+}
+
+use proto::submission::Submission;
+
 fn main() {
+    let submission = Submission::new();
     println!("Hello, world!");
 }
--- apachi_client/src/proto/submission.rs
+++ apachi_client/src/proto/submission.rs
@@ -1,4 +1,6 @@
-struct Submission {
+use rand::rngs::OsRng;
+
+pub struct Submission {
     encryption_key: &[u8; 32],
     rand_submitter: u32,
     rand_reviewer: u32,
@@ -6,6 +8,15 @@ struct Submission {
 
 impl Submission {
     pub fn new() -> Self {
-        Self {}
+        let rng = OsRng::new().expect("Failed to create OsRng instance.");
+        let encryption_key = [0u8; 32];
+        let rand_submitter = 10;
+        let rand_reviewer = 3;
+
+        Self {
+            encryption_key: &encryption_key,
+            rand_submitter,
+            rand_reviewer,
+        }
     }
 }
