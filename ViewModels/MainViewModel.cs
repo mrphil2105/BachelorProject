3fcfaa99ca069804378010093f9ef415fa5bfee7
Philip Mørch <mrphil2105@gmail.com>
2024-04-22 12:28:53 +0200

    feat: remove PageViewModel

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,42 +1,34 @@
 using Apachi.ViewModels.Auth;
 
-namespace Apachi.ViewModels
+namespace Apachi.ViewModels;
+
+public class MainViewModel : Conductor<Screen>
 {
-    public class MainViewModel : Conductor<PageViewModel>.Collection.OneActive
+    private readonly ISession _session;
+    private readonly LoginViewModel _loginViewModel;
+    private readonly RegisterViewModel _registerViewModel;
+
+    public MainViewModel(ISession session, LoginViewModel loginViewModel, RegisterViewModel registerViewModel)
     {
-        private readonly ISession _session;
-        private readonly IViewService _viewService;
-        private readonly LoginViewModel _loginViewModel;
-        private readonly RegisterViewModel _registerViewModel;
+        _session = session;
+        _loginViewModel = loginViewModel;
+        _registerViewModel = registerViewModel;
+    }
 
-        public MainViewModel(
-            ISession session,
-            IViewService viewService,
-            LoginViewModel loginViewModel,
-            RegisterViewModel registerViewModel
-        )
-        {
-            _session = session;
-            _viewService = viewService;
-            _loginViewModel = loginViewModel;
-            _registerViewModel = registerViewModel;
-        }
+    public Task GoToLogin()
+    {
+        return ActivateItemAsync(_loginViewModel);
+    }
 
-        public Task GoToLogin()
-        {
-            return ActivateItemAsync(_loginViewModel);
-        }
+    public Task GoToRegister()
+    {
+        return ActivateItemAsync(_registerViewModel);
+    }
 
-        public Task GoToRegister()
-        {
-            return ActivateItemAsync(_registerViewModel);
-        }
+    public async Task UpdateLoginState() { }
 
-        public async Task UpdateLoginState() { }
-
-        protected override Task OnInitializeAsync(CancellationToken cancellationToken)
-        {
-            return ActivateItemAsync(_loginViewModel);
-        }
+    protected override Task OnInitializeAsync(CancellationToken cancellationToken)
+    {
+        return ActivateItemAsync(_loginViewModel);
     }
 }
