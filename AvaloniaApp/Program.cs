589572759ecac2ba2f2b05fcf30cca9072d84c38
cohl7500 <cohl7500@gmail.com>
2024-04-29 14:27:29 +0200

    feat: add HPoint

--- AvaloniaApp/Program.cs
+++ AvaloniaApp/Program.cs
@@ -1,3 +1,4 @@
+using Apachi.Shared.Crypto;
 using Avalonia;
 
 namespace Apachi.AvaloniaApp;
@@ -5,7 +6,8 @@ namespace Apachi.AvaloniaApp;
 public class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     public static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
