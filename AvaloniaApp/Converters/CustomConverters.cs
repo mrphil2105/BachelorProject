a928e5d34395eab44318c8f8435e0bec3f5c9b71
Philip Mørch <mrphil2105@gmail.com>
2024-05-04 17:26:23 +0200

    feat: add converter for DateTimeOffset

---
+++ AvaloniaApp/Converters/CustomConverters.cs
@@ -0,0 +1,6 @@
+namespace Apachi.AvaloniaApp.Converters;
+
+public static class CustomConverters
+{
+    public static DateTimeOffsetConverter DateTimeOffset = new();
+}
---
+++ AvaloniaApp/Converters/DateTimeOffsetConverter.cs
@@ -0,0 +1,23 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Apachi.AvaloniaApp.Converters;
+
+public class DateTimeOffsetConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTimeOffset dateTime)
+        {
+            var humanReadable = dateTime.ToString("dd/MM/yyyy HH:mm");
+            return humanReadable;
+        }
+
+        throw new ArgumentException($"Value must be a {nameof(DateTimeOffset)}.");
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
