ab034cb6a2db1e4b9eb3fc8c3df612f3fdf75b29
Philip Mørch <mrphil2105@gmail.com>
2024-05-04 17:35:30 +0200

    feat: schedule create reviews on Reviewer registration

--- WebApi/Controllers/ReviewerController.cs
+++ WebApi/Controllers/ReviewerController.cs
@@ -1,8 +1,11 @@
 using System.Security.Cryptography;
+using Apachi.Shared;
 using Apachi.Shared.Crypto;
 using Apachi.Shared.Dtos;
 using Apachi.WebApi.Data;
+using Apachi.WebApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Apachi.WebApi.Controllers;
 
@@ -12,16 +15,19 @@ public class ReviewerController : ControllerBase
 {
     private readonly IConfiguration _configuration;
     private readonly AppDbContext _dbContext;
+    private readonly JobScheduler _jobScheduler;
     private readonly ILogger<SubmissionController> _logger;
 
     public ReviewerController(
         IConfiguration configuration,
         AppDbContext dbContext,
+        JobScheduler jobScheduler,
         ILogger<SubmissionController> logger
     )
     {
         _configuration = configuration;
         _dbContext = dbContext;
+        _jobScheduler = jobScheduler;
         _logger = logger;
     }
 
@@ -43,6 +49,15 @@ public class ReviewerController : ControllerBase
 
         _dbContext.Reviewers.Add(reviewer);
         await _dbContext.SaveChangesAsync();
+
+        var openSubmissions = _dbContext
+            .Submissions.Where(submission => submission.Status == SubmissionStatus.Open)
+            .AsAsyncEnumerable();
+
+        await foreach (var openSubmission in openSubmissions)
+        {
+            await _jobScheduler.ScheduleJobAsync(JobType.CreateReviews, openSubmission.Id.ToString());
+        }
 
         var reviewerEncryptedSharedKey = EncryptionUtils.AsymmetricEncrypt(sharedKey, registerDto.ReviewerPublicKey);
         var registeredDto = new ReviewerRegisteredDto(reviewer.Id, reviewerEncryptedSharedKey);
