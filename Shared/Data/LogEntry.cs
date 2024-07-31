add683b7c7c1cfd213e2c0a5d0b5fac9fd285f8f
Philip Mørch <mrphil2105@gmail.com>
2024-08-01 00:05:17 +0200

    feat: update LogEntry and remove util functions

--- Shared/Data/LogEntry.cs
+++ Shared/Data/LogEntry.cs
@@ -4,11 +4,9 @@ public class LogEntry
 {
     public Guid Id { get; set; }
 
-    public required Guid SubmissionId { get; set; }
-
     public required ProtocolStep Step { get; set; }
 
-    public required byte[] MessageBytes { get; set; }
+    public required byte[] Data { get; set; }
 
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 }
--- Shared/Data/LogEntryResult.cs
+++
@@ -1,6 +0,0 @@
-using Apachi.Shared.Data.Messages;
-
-namespace Apachi.Shared.Data;
-
-public record LogEntryResult<TMessage>(Guid Id, Guid SubmissionId, TMessage Message, DateTime CreatedDate)
-    where TMessage : IMessage;
