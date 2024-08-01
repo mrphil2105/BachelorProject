d3669178949452992aeefda6717d2023196de9f0
COHL7500 <cohl7500@gmail.com>
2024-08-01 12:37:59 +0200

    feat: implement lipmaa decomp

--- Shared/Crypto/SetMemberProof.cs
+++ Shared/Crypto/SetMemberProof.cs
@@ -1,7 +1,4 @@
-using System.Security.Cryptography;
-using Org.BouncyCastle.Asn1.Nist;
 using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Math.EC;
 
 namespace Apachi.Shared.Crypto;
 
@@ -12,7 +9,8 @@ public class SetMemberProof
 {
     private readonly BigInteger _a;
     private readonly BigInteger _b;
-    
+    private readonly List<int> grades = [2, 4, 7, 10, 12];
+
     public SetMemberProof(BigInteger a, BigInteger b)
     {
         _a = a;
@@ -21,38 +19,56 @@ public class SetMemberProof
 
     public bool Verify(Commitment commitment, BigInteger randomness, BigInteger x)
     {
-        
+
         bool isInRange = x.CompareTo(_a) >= 0 &&
                          x.CompareTo(_b) <= 0;
-        
+
         bool matchesValue = commitment.MatchesValue(x.ToByteArray(), randomness);
-        
+
         return isInRange && matchesValue;
     }
-    
+
     public static SetMemberProof Create(BigInteger a, BigInteger b)
     {
         return new SetMemberProof(a, b);
     }
-    
+
     public byte[] ToBytes()
     {
         return DataUtils.SerializeBigIntegers(_a, _b);
     }
-    
+
     public static SetMemberProof FromBytes(byte[] bytes)
     {
         var integers = DataUtils.DeserializeBigIntegers(bytes);
         return new SetMemberProof(integers[0], integers[1]);
     }
 
-    private static BigInteger[] DecomposeIntoPowersOf2(BigInteger x)
+    private static (BigInteger, BigInteger, BigInteger, BigInteger) LipmaaDecomp(BigInteger x)
     {
-        var x0 = x.And(BigInteger.One);
-        var x1 = x.And(BigInteger.Two);
-        var x2 = x.And(BigInteger.One.ShiftLeft(2));
-        var x3 = x.And(BigInteger.One.ShiftLeft(3));
+        BigInteger upperBound1 = new BigInteger("100000");
+        BigInteger upperBound2 = new BigInteger("1000");
+        BigInteger upperBound3 = new BigInteger("100");
+        BigInteger upperBound4 = new BigInteger("10");
 
-        return [x0, x1, x2, x3];
+        for (BigInteger i = BigInteger.Zero; i.CompareTo(upperBound1) < 0; i = i.Add(BigInteger.One))
+        {
+            for (BigInteger j = BigInteger.Zero; j.CompareTo(upperBound2) < 0; j = j.Add(BigInteger.One))
+            {
+                for (BigInteger k = BigInteger.Zero; k.CompareTo(upperBound3) < 0; k = k.Add(BigInteger.One))
+                {
+                    for (BigInteger l = BigInteger.Zero; l.CompareTo(upperBound4) < 0; l = l.Add(BigInteger.One))
+                    {
+                        BigInteger sumOfSquares = i.Pow(2).Add(j.Pow(2)).Add(k.Pow(2)).Add(l.Pow(2));
+                        if (sumOfSquares.Equals(x))
+                        {
+                            return (i, j, k, l);
+                        }
+                    }
+                }
+            }
+        }
+
+        return (BigInteger.Zero, BigInteger.Zero, BigInteger.Zero, BigInteger.Zero);
     }
 }
