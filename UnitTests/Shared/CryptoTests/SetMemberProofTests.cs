1b6ad9a0fc78d610142509191a092426bc2a75a2
Philip Mørch <mrphil2105@gmail.com>
2024-08-11 00:37:43 +0200

    test: set member proof serialization

--- UnitTests/Shared/CryptoTests/SetMemberProofTests.cs
+++ UnitTests/Shared/CryptoTests/SetMemberProofTests.cs
@@ -1,60 +1,56 @@
-using System.Runtime.InteropServices.JavaScript;
+using System.Reflection;
 using Apachi.Shared.Crypto;
-using Org.BouncyCastle.Asn1;
-using Org.BouncyCastle.Asn1.Nist;
 using Org.BouncyCastle.Math;
 
 namespace Apachi.UnitTests.Shared.CryptoTests;
 
 public class SetMemberProofTests
 {
-    
-    private readonly List<(BigInteger g, BigInteger g_r)> _gradesList;
-    
+    private readonly List<(BigInteger g, BigInteger g_r)> _grades;
+
     public SetMemberProofTests()
     {
-        _gradesList = new List<(BigInteger g, BigInteger g_r)>
+        _grades = new List<(BigInteger g, BigInteger g_r)>
         {
             (BigInteger.One, DataUtils.GenerateBigInteger()),
             (BigInteger.Two, DataUtils.GenerateBigInteger()),
             (BigInteger.Three, DataUtils.GenerateBigInteger())
         };
     }
-    
+
     [Fact]
     public void Verify_ValidProof_ReturnsTrue()
     {
-        
         var x = BigInteger.Two;
-        
-        var proof = SetMemberProof.Create(_gradesList);
-        
+
+        var proof = SetMemberProof.Create(_grades);
+
         var result = proof.Verify(x);
-        
-        Assert.True(result);
+
+        result.Should().BeTrue();
     }
-    
+
     [Fact]
     public void Verify_InvalidProof_ReturnsFalse()
     {
         var x = BigInteger.Five;
-        
-        var proof = SetMemberProof.Create(_gradesList);
-        
+
+        var proof = SetMemberProof.Create(_grades);
+
         var result = proof.Verify(x);
 
         result.Should().BeFalse();
     }
-    
+
     [Fact]
     public void Verify_EmptySet_ReturnsFalse()
     {
         var emptyGradesList = new List<(BigInteger g, BigInteger g_r)>();
-        
+
         var x = BigInteger.One;
-        
+
         var proof = SetMemberProof.Create(emptyGradesList);
-        
+
         var result = proof.Verify(x);
 
         result.Should().BeFalse();
@@ -63,27 +59,29 @@ public class SetMemberProofTests
     [Fact]
     public void Verify_BoundaryValues_ReturnsExpectedResults()
     {
-        var proof = SetMemberProof.Create(_gradesList);
-        
+        var proof = SetMemberProof.Create(_grades);
+
         var result1 = proof.Verify(BigInteger.One);
         var result2 = proof.Verify(BigInteger.Two);
         var result3 = proof.Verify(BigInteger.Three);
-        
+
         result1.Should().BeTrue();
         result2.Should().BeTrue();
         result3.Should().BeFalse();
     }
-    
-    
-    /*
+
     [Fact]
     public void FromBytes_ToBytes_ReturnsExpectedResults()
     {
-        var proof = SetMemberProof.Create(_gradesList);
+        var field = typeof(SetMemberProof).GetField("_grades", BindingFlags.Instance | BindingFlags.NonPublic)!;
+        var proof = SetMemberProof.Create(_grades);
         var bytes = proof.ToBytes();
-        var deserializedProof = SetMemberProof.FromBytes(bytes); ;
+        var deserializedProof = SetMemberProof.FromBytes(bytes);
 
-        proof.Equals(deserializedProof).Should().BeTrue();
+        var proofList = (List<(BigInteger, BigInteger)>)field.GetValue(proof)!;
+        var deserializedProofList = (List<(BigInteger, BigInteger)>)field.GetValue(deserializedProof)!;
+
+        proofList.Should().Equal(deserializedProofList);
     }
-    */
 }
+
