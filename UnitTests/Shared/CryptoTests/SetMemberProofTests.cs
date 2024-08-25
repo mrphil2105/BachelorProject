3c1a17b2ebb06992b92f544c7a3673a88aa4bb60
Philip Mørch <mrphil2105@gmail.com>
2024-08-25 23:42:43 +0200

    test: update tests for set membership proof

--- UnitTests/Shared/CryptoTests/SetMemberProofTests.cs
+++ UnitTests/Shared/CryptoTests/SetMemberProofTests.cs
@@ -1,31 +1,24 @@
 using System.Reflection;
+using System.Security.Cryptography;
 using Apachi.Shared.Crypto;
 using Org.BouncyCastle.Math;
+using ECPoint = Org.BouncyCastle.Math.EC.ECPoint;
 
 namespace Apachi.UnitTests.Shared.CryptoTests;
 
 public class SetMemberProofTests
 {
-    private readonly List<(BigInteger g, BigInteger g_r)> _grades;
+    private readonly BigInteger _size = new("8");
 
-    public SetMemberProofTests()
-    {
-        _grades = new List<(BigInteger g, BigInteger g_r)>
-        {
-            (BigInteger.One, DataUtils.GenerateBigInteger()),
-            (BigInteger.Two, DataUtils.GenerateBigInteger()),
-            (BigInteger.Three, DataUtils.GenerateBigInteger())
-        };
-    }
+    public SetMemberProofTests() { }
 
     [Fact]
     public void Verify_ValidProof_ReturnsTrue()
     {
-        var x = BigInteger.Two;
+        var index = BigInteger.Five;
 
-        var proof = SetMemberProof.Create(_grades);
-
-        var result = proof.Verify(x);
+        var proof = SetMemberProof.Create(index, _size);
+        var result = proof.Verify(_size);
 
         result.Should().BeTrue();
     }
@@ -33,55 +26,46 @@ public class SetMemberProofTests
     [Fact]
     public void Verify_InvalidProof_ReturnsFalse()
     {
-        var x = BigInteger.Five;
+        var index = _size;
 
-        var proof = SetMemberProof.Create(_grades);
+        Action action = () => SetMemberProof.Create(index, _size);
 
-        var result = proof.Verify(x);
-
-        result.Should().BeFalse();
+        action
+            .Should()
+            .ThrowExactly<CryptographicException>()
+            .WithMessage(
+                $"Unable to create proof for {index.Add(BigInteger.One)} within range 0 to {_size.Add(BigInteger.One)}."
+            );
     }
 
     [Fact]
     public void Verify_EmptySet_ReturnsFalse()
     {
-        var emptyGradesList = new List<(BigInteger g, BigInteger g_r)>();
+        var index = BigInteger.One;
 
-        var x = BigInteger.One;
-
-        var proof = SetMemberProof.Create(emptyGradesList);
-
-        var result = proof.Verify(x);
+        var proof = SetMemberProof.Create(index, _size);
+        var result = proof.Verify(index);
 
         result.Should().BeFalse();
     }
 
     [Fact]
-    public void Verify_BoundaryValues_ReturnsExpectedResults()
-    {
-        var proof = SetMemberProof.Create(_grades);
-
-        var result1 = proof.Verify(BigInteger.One);
-        var result2 = proof.Verify(BigInteger.Two);
-        var result3 = proof.Verify(BigInteger.Three);
-
-        result1.Should().BeTrue();
-        result2.Should().BeTrue();
-        result3.Should().BeFalse();
-    }
-
-    [Fact]
     public void FromBytes_ToBytes_ReturnsExpectedResults()
     {
-        var field = typeof(SetMemberProof).GetField("_grades", BindingFlags.Instance | BindingFlags.NonPublic)!;
-        var proof = SetMemberProof.Create(_grades);
+        var c1Field = typeof(SetMemberProof).GetField("_c1", BindingFlags.Instance | BindingFlags.NonPublic)!;
+        var c2Field = typeof(SetMemberProof).GetField("_c2", BindingFlags.Instance | BindingFlags.NonPublic)!;
+
+        var index = BigInteger.Six;
+        var proof = SetMemberProof.Create(index, _size);
         var bytes = proof.ToBytes();
         var deserializedProof = SetMemberProof.FromBytes(bytes);
 
-        var proofList = (List<(BigInteger, BigInteger)>)field.GetValue(proof)!;
-        var deserializedProofList = (List<(BigInteger, BigInteger)>)field.GetValue(deserializedProof)!;
+        var c1 = (ECPoint)c1Field.GetValue(proof)!;
+        var c2 = (ECPoint)c2Field.GetValue(proof)!;
+        var deserializedC1 = (ECPoint)c1Field.GetValue(deserializedProof)!;
+        var deserializedC2 = (ECPoint)c2Field.GetValue(deserializedProof)!;
 
-        proofList.Should().Equal(deserializedProofList);
+        c1.Should().Be(deserializedC1);
+        c2.Should().Be(deserializedC2);
     }
 }
-
